94da5c3e6a56a92b591d4840e49498dfe930deae
Scott Miller <scott.miller171@gmail.com>
2015-09-24 15:00:52 -0400

    run docker deploy in script

---
+++ docker-deploy.sh
@@ -0,0 +1,4 @@
+/bin/bash -e
+test $TRAVIS_PULL_REQUEST == "false" && test $TRAVIS_BRANCH == "master"
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
+docker push $DOCKER_USER/ssh-chat:latest
