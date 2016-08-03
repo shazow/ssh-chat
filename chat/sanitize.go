07808f3ee86a6cecf1280dbde079fb162c70b5f9
A. Campbell <allenc28@yahoo.com>
2016-08-03 12:07:55 -0400

    Limit user name length

--- chat/sanitize.go
+++ chat/sanitize.go
@@ -3,10 +3,17 @@ package chat
 import "regexp"
 
 var reStripName = regexp.MustCompile("[^\\w.-]")
+const maxLength = 16
 
-// SanitizeName returns a name with only allowed characters.
+// SanitizeName returns a name with only allowed characters and a reasonable length
 func SanitizeName(s string) string {
-	return reStripName.ReplaceAllString(s, "")
+	s = reStripName.ReplaceAllString(s, "")
+	nameLength := maxLength
+	if len(s) <= maxLength {
+		nameLength = len(s)
+	}
+	s = s[:nameLength]
+	return s
 }
 
 var reStripData = regexp.MustCompile("[^[:ascii:]]")
