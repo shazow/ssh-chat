c87073edaee0a11370d8fc801036856380be74f1
UlisseMini <ulisse.mini@gmail.com>
2019-01-25 12:07:34 -0500

    Better comments

--- godoc.go
+++ godoc.go
@@ -1,5 +1,5 @@
 /*
-sshchat package is an implementation of an ssh server which serves a chat room
+Package sshchat is an implementation of an ssh server which serves a chat room
 instead of a shell.
 
 sshd subdirectory contains the ssh-related pieces which know nothing about chat.
